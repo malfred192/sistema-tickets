96377e4179c994c6b10c4f0b9735a14cc55f7e2a
Manuel Hernández <malfred192@gmail.com>
2021-04-15 15:56:17 -0600

    Agregar la funcionalidad de consultar los archivos adjuntos al tickets

--- api/app/Http/Controllers/FileController.php
+++ api/app/Http/Controllers/FileController.php
@@ -9,7 +9,7 @@ class FileController extends Controller
 {
     public function anexos_ticket(Request $file){
         $file=Storage::disk('public/anexos_tickets')->get('anexos_tickets/ZbtF5WR6yN6c1zJDn74vv3ybjpIOniAKJUWWGwqi.pdf');
-        dd($file);
+        //dd($file);
         //return response()->download(public_path($file->ruta));
     }
 }
